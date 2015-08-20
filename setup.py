a41898c6ce7c8169883c8ffca64990934e51b063
Kyle Richardson <kylerichardson@users.noreply.github.com>
2015-08-20 16:05:55 -0400

    Initial project commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import sys
+from setuptools.command.test import test as TestCommand
+from setuptools import setup
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initalize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='whoisapi',
+    version='0.0.1',
+    description='Wrapper for whoisxmlapi web service',
+    author='Kyle Richardson',
+    author_email='kylerichardson2@gmail.com',
+    license='MIT',
+    packages='whoisapi',
+    install_requires=['requests'],
+    tests_requires=['pytest'],
+    cmdclass={'test':PyTest},
+)
+
---
+++ whoisapi/whoisapi.py
@@ -0,0 +1,5 @@
+import json
+import requests
+
+BASE_URL = r'https://www.whoisxmlapi.com/whoisserver/WhoisService?outputFormat=JSON&domain='
+
